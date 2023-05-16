4cd2c761ab6d6902f186a9dd23487d2d6580af9f
Suyoung Seo <32202145@dankook.ac.kr>
2023-05-17 02:39:46 +0900

    MyPet 1.1.1, 검색 조건 수정 (상세 정보 작성자 조건 제거)

--- pet/views/base_views.py
+++ pet/views/base_views.py
@@ -14,8 +14,7 @@ def index(request):
             Q(name__icontains=kw) |  # 이름 검색
             Q(species__icontains=kw) |  # 종류 검색
             Q(detailinfo__content__icontains=kw) |  # 상세 정보 검색
-            Q(author__username__icontains=kw) |  # 펫 작성자 검색
-            Q(detailinfo__author__username__icontains=kw)  # 상세 정보 작성자 검색
+            Q(author__username__icontains=kw)  # 펫 작성자 검색
         ).distinct()
     paginator = Paginator(baseInfo_list, 5)  # 페이지당 5개씩 보여주기
     page_obj = paginator.get_page(page)
